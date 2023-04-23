dbbf61052c9f72cf164deb41f48547bbb5a105c2
Ahsan Ali <ahsanbutt515@icloud.com>
2023-04-24 01:18:36 +0500

    LayoutCompletedSiteDone

--- src/components/TransitionEffect.js
+++ src/components/TransitionEffect.js
@@ -7,11 +7,11 @@ const TransitionEffect = () => {
         className="fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-primary"
         initial={{
           x: "100%",
-          y: "100%",
+          width: "100%",
         }}
-        animatte={{
+        animate={{
           x: "0%",
-          y: "0%",
+          width: "0%",
         }}
         exit={{
           x: ["0%", "100%"],
@@ -26,11 +26,11 @@ const TransitionEffect = () => {
         className="fixed top-0 bottom-0 right-full w-screen h-screen z-20 bg-light"
         initial={{
           x: "100%",
-          y: "100%",
+          width: "100%",
         }}
-        animatte={{
+        animate={{
           x: "0%",
-          y: "0%",
+          width: "0%",
         }}
         transition={{
           delay: 0.2,
@@ -42,11 +42,11 @@ const TransitionEffect = () => {
         className="fixed top-0 bottom-0 right-full w-screen h-screen z-10 bg-dark"
         initial={{
           x: "100%",
-          y: "100%",
+          width: "100%",
         }}
-        animatte={{
+        animate={{
           x: "0%",
-          y: "0%",
+          width: "0%",
         }}
         transition={{
           delay: 0.4,
